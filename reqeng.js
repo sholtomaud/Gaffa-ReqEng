a099cc014575ce4243776275f8947dbd65c9d3c2
Sholto Maud <sholto.maud@gmail.com>
2014-05-12 10:31:01 +1000

    now populate array, then write to file

--- reqeng.js
+++ reqeng.js
@@ -11,20 +11,35 @@ var xlsxj = require("xlsx-to-json"),
 Converter = require("csvtojson").core.Converter,
 fs = require("fs"),
 inquirer = require("inquirer"),
-glob = require("glob");
+glob = require("glob"),
+page = './pages/output.js',
+page2 = './pages/pageOut.js',
+json = './json/json.json';
 
 console.log("Generate Gaffa code from .csv requirements file");
-
-
-
-
 
 
 function getForm(){
 
 	var formJSON ={};
 
-	glob("*.csv", function (err,files){
+	fs.unlink(page, function (err) {
+    if (err) throw err;
+    console.log('successfully deleted ['+page+']');
+  });
+  
+  fs.unlink(page2, function (err) {
+    if (err) throw err;
+    console.log('successfully deleted ['+page2+']');
+  });
+  
+  fs.unlink(json, function (err) {
+    if (err) throw err;
+    console.log('successfully deleted ['+json+']');
+  });
+  
+  
+  glob("*.csv", function (err,files){
 			
 	var questions = [
 		{
@@ -51,61 +66,93 @@ function getForm(){
 		var csvFileName = selected.file;
 		var app = './pages/' + selected.appName + '.js';
 		
+           
 		var fileStream = fs.createReadStream(csvFileName);
 
 		//new converter instance
 		var csvConverter = new Converter({constructResult:true});
 
+    var pageArray = new Array;
 
 		//end_parsed will be emitted once parsing finished
 		csvConverter.on("end_parsed",function(jsonObj){
 		  //console.log(jsonObj); //here is your result json object
 			
-			console.log('jsonObj end'); //here is your result json object
+			//console.log('jsonObj end'); //here is your result json object
 			//console.log(jsonObj); //here is your result json object
 
-		  fs.appendFile('json.json', JSON.stringify(jsonObj), function (err) {
+		  fs.appendFile(json, JSON.stringify(jsonObj), function (err) {
       	if (err) throw err;
 			});
 		  
-//		  label,type,class,rows,cols,postLabel,placeholder,maxLength,size,required,validation,lookups,binding,comment
+      
+      
+      //label,type,class,rows,cols,postLabel,placeholder,maxLength,size,required,validation,lookups,binding,comment
 
-		  fs.appendFile('output.js', 'function createForm(){\n  var formPage = new views.Form();\n', function (err) {
+		  fs.appendFile(page, 'function createForm(){\n  var formPage = new views.Form();\n', function (err) {
       	if (err) throw err;
 			});
 	
-
+      pageArray.push('function createForm(){\n  var formPage = new views.Form();\n');
+    
 			//jsonObj.forEach(function(key) {
-      //  fs.appendFile('output.js', 'var ' + key.label.replace(/\s/g, '') + ' = new views.Label();\n', function (err) {
+      //  fs.appendFile(page, 'var ' + key.label.replace(/\s/g, '') + ' = new views.Label();\n', function (err) {
       //  	if (err) throw err;
 		//		});
 		//	});
 
 			jsonObj.forEach(function(key) {
-        fs.appendFile('output.js', '  var ' + key.label.replace(/\s/g, '') + 'Label = new views.Label();\n  var ' + key.label.replace(/\s/g, '') + key.type + ' = new views.'+key.type+'();\n\n', function (err) {
+        fs.appendFile(page, '  var ' + key.label.replace(/\s/g, '') + 'Label = new views.Label();\n  var ' + key.label.replace(/\s/g, '') + key.type + ' = new views.'+key.type+'();\n\n', function (err) {
         	if (err) throw err;
 				});
+        
+        pageArray.push('  var ' + key.label.replace(/\s/g, '') + 'Label = new views.Label();\n  var ' + key.label.replace(/\s/g, '') + key.type + ' = new views.'+key.type+'();\n\n');
+        
 			});
 
 			jsonObj.forEach(function(key) {
-        fs.appendFile('output.js', key.label.replace(/\s/g, '') + '.text.value = \''+ key.label +'\';\n' + key.label.replace(/\s/g, '') + '.classes.value = \''+ key.class +'\';\n\n', function (err) {
+        fs.appendFile(page, key.label.replace(/\s/g, '') + '.text.value = \''+ key.label +'\';\n' + key.label.replace(/\s/g, '') + '.classes.value = \''+ key.classes +'\';\n\n', function (err) {
         	if (err) throw err;
 				});
+        
+        pageArray.push(key.label.replace(/\s/g, '') + '.text.value = \''+ key.label +'\';\n' + key.label.replace(/\s/g, '') + '.classes.value = \''+ key.classes +'\';\n');
 			});
 
-		  fs.appendFile('output.js', '  formPage.views.content.add([\n', function (err) {
+      
+      
+      
+		  fs.appendFile(page, '  formPage.views.content.add([\n', function (err) {
       	if (err) throw err;
 			});
-
+      pageArray.push('  formPage.views.content.add([\n');
+      
+      
 		  jsonObj.forEach(function(key) {
-        fs.appendFile('output.js', key.label.replace(/\s/g, '') + 'Label,\n' + key.label.replace(/\s/g, '') + key.type + ',\n', function (err) {
+        fs.appendFile(page, key.label.replace(/\s/g, '') + 'Label,\n' + key.label.replace(/\s/g, '') + key.type + ',\n', function (err) {
         	if (err) throw err;
 				});
+        
+        pageArray.push(key.label.replace(/\s/g, '') + 'Label,\n' + key.label.replace(/\s/g, '') + key.type + ',\n');
+        
 			});
 
-		  fs.appendFile('output.js', '  ]);\n  formPage.path = \'[/form]\';', function (err) {
+		  fs.appendFile(page, '  ]);\n  formPage.path = \'[/form]\';', function (err) {
       	if (err) throw err;
 			});
+      
+      pageArray.push('  ]);\n  formPage.path = \'[/form]\';');
+      
+      
+      fs.writeFile(
+         page2 ,
+         //pageArray.map(function(v){ return v.join(', ')).join('\n'),
+         pageArray.join('\n'),
+         function (err) { console.log(err ? 'Error :' + err : 'ok - writeFile ['+page2+']') }
+      );
+      
+      
+      
+      
 
 
 /*
@@ -171,8 +218,8 @@ function getForm(){
 function runApp(){
 
 	var formJSON = getForm();
-	console.log('formJSON');
-	console.log(formJSON);
+	//console.log('formJSON');
+	//console.log(formJSON);
 
 /*
 fs.writeFile(app,JSON.stringify(jsonObj), function (err) {
