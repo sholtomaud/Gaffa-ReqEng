3a7df042e26a5734ac203740dc4f2f326e794aca
Sholto Maud <sholto.maud@gmail.com>
2014-05-12 16:40:55 +1000

    working form except for textarea

--- reqeng.js
+++ reqeng.js
@@ -20,141 +20,229 @@ console.log("Generate Gaffa code from .csv requirements file");
 
 
 function getForm(){
-
 	var formJSON ={};
-
-	fs.unlink(page, function (err) {
-    if (err) throw err;
-    console.log('successfully deleted ['+page+']');
-  });
-  
-  fs.unlink(page2, function (err) {
-    if (err) throw err;
-    console.log('successfully deleted ['+page2+']');
-  });
-  
-  fs.unlink(json, function (err) {
-    if (err) throw err;
-    console.log('successfully deleted ['+json+']');
-  });
-  
   
   glob("*.csv", function (err,files){
 			
-	var questions = [
-		{
-			type: "input",
-	    name: "appName",
-	    message: "What is the name of your page?",
-	  },
-		{
-			type: "list",
-	    name: "file",
-	    message: "What .csv file would you like to use??",
-	    choices: files,
-	    when: function( answers ) {
-	      return answers.comments !== "Nope, all good!";
-	    }
-	}];	
-	
-	inquirer.prompt(questions, function( selected ) {
-	  // Use user feedback for... whatever!!
-	  //console.log("\nReturn:");
-	  //  console.log(JSON.stringify(selected,null, "  "));
+    var questions = [
+      {
+        type: "input",
+        name: "appName",
+        message: "What is the name of your page?",
+      },
+      {
+        type: "list",
+        name: "file",
+        message: "What .csv file would you like to use??",
+        choices: files,
+        when: function( answers ) {
+          return answers.comments !== "Nope, all good!";
+        }
+    }];	
+    
+    inquirer.prompt(questions, function( selected ) {
+      // Use user feedback for... whatever!!
+      //console.log("\nReturn:");
+      //  console.log(JSON.stringify(selected,null, "  "));
 
+      var csvFileName = selected.file;
+      var app = './pages/' + selected.appName + '.js';
+      
+      fs.unlink(app, function (err) {
+        if (err) throw err;
+        console.log('successfully deleted ['+app+']');
+      });
+              
+      var fileStream = fs.createReadStream(csvFileName);
 
-		var csvFileName = selected.file;
-		var app = './pages/' + selected.appName + '.js';
-		
-           
-		var fileStream = fs.createReadStream(csvFileName);
+      //new converter instance
+      var csvConverter = new Converter({constructResult:true});
+      var pageArray = new Array;
 
-		//new converter instance
-		var csvConverter = new Converter({constructResult:true});
-
-    var pageArray = new Array;
-
-		//end_parsed will be emitted once parsing finished
-		csvConverter.on("end_parsed",function(jsonObj){
-		  //console.log(jsonObj); //here is your result json object
-			
-			//console.log('jsonObj end'); //here is your result json object
-			//console.log(jsonObj); //here is your result json object
-
+      //end_parsed will be emitted once parsing finished
+      csvConverter.on("end_parsed",function(jsonObj){
+		  
 		  fs.appendFile(json, JSON.stringify(jsonObj), function (err) {
       	if (err) throw err;
 			});
 		  
-      
-      
       //label,type,class,rows,cols,postLabel,placeholder,maxLength,size,required,validation,lookups,binding,comment
 
-		  fs.appendFile(page, 'function createForm(){\n  var formPage = new views.Form();\n', function (err) {
-      	if (err) throw err;
-			});
 	
-      pageArray.push('function createForm(){\n  var formPage = new views.Form();\n');
+        //module export
+        pageArray.push('module.exports = function(app){');
+        pageArray.push('var views = app.views;');
+        pageArray.push('var action = app.actions;');
+        pageArray.push('var behaviours = app.behaviours;');
+        
+        //function
+        pageArray.push(' function createForm(){');
+        //pageArray.push('   var formPage = new views.Form();');
+        
     
 			//jsonObj.forEach(function(key) {
       //  fs.appendFile(page, 'var ' + key.label.replace(/\s/g, '') + ' = new views.Label();\n', function (err) {
       //  	if (err) throw err;
 		//		});
 		//	});
-
-			jsonObj.forEach(function(key) {
-        fs.appendFile(page, '  var ' + key.label.replace(/\s/g, '') + 'Label = new views.Label();\n  var ' + key.label.replace(/\s/g, '') + key.type + ' = new views.'+key.type+'();\n\n', function (err) {
-        	if (err) throw err;
-				});
+      
+        //Array for field container
+        var fieldArray = new Array;
         
-        pageArray.push('  var ' + key.label.replace(/\s/g, '') + 'Label = new views.Label();\n  var ' + key.label.replace(/\s/g, '') + key.type + ' = new views.'+key.type+'();\n\n');
+
+        jsonObj.forEach(function(key) {
+          var normalisedLabel = key.label.replace(/\s|\(|\)|\/|\?|\\/g, '');
+          
+          //var for label 
+          var label = normalisedLabel + 'Label';
+          pageArray.push('  var ' + label +' = new views.Label();');
+          
+           
+          //var for inputType (textbox,dropdown etc)
+          var inputType = normalisedLabel + ucfirst(key.type);
+          
+          if (key.type.toLowerCase() == 'date'){
+            pageArray.push('  var ' + inputType + ' = new views.Textbox();');
+            pageArray.push('  ' + inputType + '.type.value = \'date\'');
+            
+          }
+          else if ( key.type.toLowerCase() == 'radio'){
+            pageArray.push('  var ' + inputType + ' = new views.'+ucfirst(key.type)+'();');
+          }
+          else{
+            pageArray.push('  var ' + inputType + ' = new views.'+ucfirst(key.type)+'();');
+          }
+          
+          if ('undefined' !== typeof key.binding && key.binding !== '' && key.binding !== ' ' ) {
+            pageArray.push('  ' + inputType + '.value.binding = \'['+ key.binding +']\''); //'[basin]';
+          }
+          
+          if ('undefined' !== typeof key.size && key.size !== '' && key.size !== ' ' && key.type !== 'select') {
+            var size = Number(key.size);
+            size = size + 2;
+            pageArray.push('  ' + inputType + '.size.value = '+ size +';');
+          }
+          
+          if ( key.type.toLowerCase() == 'select' ){ //Create select box options
+            var options = key.options.split(',');
+            pageArray.push('  ' + inputType + '.options.value = [\''+options.join("','")+'\'];');
+          }
+          
+          if ( key.type.toLowerCase() == 'textarea' ){ //Create select box options
+            pageArray.push('  ' + inputType + '.cols.value = '+key.cols+';');
+            pageArray.push('  ' + inputType + '.rows.value = '+key.rows+';');
+          }
+          
+          
+          //set label text value
+          var labelText = '\'' + key.label + '\'';
+          pageArray.push( '  ' + label + '.text.value = '+ labelText+';' );
+          
+          //set maxLength text value
+          if ('undefined' !== typeof key.maxLength && key.maxLength !== '' && key.maxLength !== ' ' && key.type.toLowerCase() !== 'select') {
+            pageArray.push('  ' + inputType + '.maxLength.value = '+key.maxLength +';');
+          }
+          
+          //set postLabel text value
+          if ('undefined' !== typeof key.postLabel && key.postLabel !== '' && key.postLabel !== ' ') {
+            var pstLabel = normalisedLabel + 'PostLabel';
+           
+            pageArray.push('  var ' + pstLabel +' = new views.Label();');
+            var postLabelText = '\'' + key.postLabel + '\'';
+            pageArray.push( '  ' + pstLabel + '.text.value = '+ postLabelText+';' );
+            
+            //set pstLabel classes
+            var inputlabelClasses = '\''+ key.classes +'\'';
+            pageArray.push( '  ' + pstLabel + '.classes.value = \'postlab\';\n');
+          }
+          
+          //set label classes
+          var inputlabelClasses = '\''+ key.classes +'\'';
+          pageArray.push( '  ' + label + '.classes.value = \'fldname\';\n');
+          
+          //set input classes
+          var inputClasses = '\''+ key.classes +'\'';
+          pageArray.push( '  ' + inputType + '.classes.value = ' + inputClasses +';\n');
+          
+          var fieldContainer = label + 'Container';
+          pageArray.push('  var ' + fieldContainer + ' = new views.Container();');
+          pageArray.push('  ' + fieldContainer + '.views.content.add([');
+          pageArray.push('    ' + label + ',');
+          pageArray.push('    ' + inputType + ',');
+          if ('undefined' !== typeof key.postLabel && key.postLabel !== '' && key.postLabel !== ' ') { pageArray.push('    ' + pstLabel + ',') };
+          pageArray.push('  ]);\n');
+          pageArray.push('  ' + fieldContainer +'.classes.value = \'field\'');
+          fieldArray.push(fieldContainer);
+          
+        });
         
-			});
-
-			jsonObj.forEach(function(key) {
-        fs.appendFile(page, key.label.replace(/\s/g, '') + '.text.value = \''+ key.label +'\';\n' + key.label.replace(/\s/g, '') + '.classes.value = \''+ key.classes +'\';\n\n', function (err) {
-        	if (err) throw err;
-				});
+        //add to page view
+        pageArray.push('   var formTemplate = new views.Container();');
         
-        pageArray.push(key.label.replace(/\s/g, '') + '.text.value = \''+ key.label +'\';\n' + key.label.replace(/\s/g, '') + '.classes.value = \''+ key.classes +'\';\n');
-			});
-
-      
-      
-      
-		  fs.appendFile(page, '  formPage.views.content.add([\n', function (err) {
-      	if (err) throw err;
-			});
-      pageArray.push('  formPage.views.content.add([\n');
-      
-      
-		  jsonObj.forEach(function(key) {
-        fs.appendFile(page, key.label.replace(/\s/g, '') + 'Label,\n' + key.label.replace(/\s/g, '') + key.type + ',\n', function (err) {
-        	if (err) throw err;
-				});
+        var formTemplate = new Array;
+        pageArray.push('    formTemplate.views.content.add([');
         
-        pageArray.push(key.label.replace(/\s/g, '') + 'Label,\n' + key.label.replace(/\s/g, '') + key.type + ',\n');
+        var fieldArrayLength = fieldArray.length;
+        for (var i = 0; i < fieldArrayLength; i++) {
+            var container;
+            if (i == (fieldArrayLength-1)){
+              container = fieldArray[i];
+            }
+            else{
+              container = fieldArray[i] +',';
+            }
+            pageArray.push('      ' + container);
+        }
         
-			});
+        pageArray.push('    ]);\n  formTemplate.path = \'[/form]\';');
+        
+        pageArray.push('    return formTemplate;');
+        pageArray.push('  }'); //end function
+        
+        pageArray.push('function createView(){');
+        pageArray.push('var appView = new views.Container();');
 
-		  fs.appendFile(page, '  ]);\n  formPage.path = \'[/form]\';', function (err) {
-      	if (err) throw err;
-			});
-      
-      pageArray.push('  ]);\n  formPage.path = \'[/form]\';');
-      
-      
-      fs.writeFile(
-         page2 ,
-         //pageArray.map(function(v){ return v.join(', ')).join('\n'),
-         pageArray.join('\n'),
-         function (err) { console.log(err ? 'Error :' + err : 'ok - writeFile ['+page2+']') }
-      );
-      
-      
-      
-      
+        pageArray.push('appView.views.content.add([');
+            //createHeader(),
+        pageArray.push('  createForm(),');
+            //stickyFooter()
+        pageArray.push(']);');
 
+        pageArray.push('appView.classes.value = \'app\';');
+        //pageArray.push('appView.behaviours = createAppBehaviours();');
 
+        pageArray.push('return appView;');
+        pageArray.push('}');//end createView()
+        
+        pageArray.push('return createView;');
+        pageArray.push('}'); //end module
+/*        
+        pageArray.push('  formTemplate.views.content.add([');
+        
+        jsonObj.forEach(function(key) {
+          var label = key.label.replace(/\s/g, '') + 'Label';
+          pageArray.push('    ' + label +',');
+          
+          var type = key.label.replace(/\s/g, '') + key.type;
+          pageArray.push('    ' + type + ',');
+          
+        });
+
+        pageArray.push('  ]);\n  formTemplate.path = \'[/form]\';');
+        
+        pageArray.push('}');
+  
+        pageArray.push(fieldArray);
+        pageArray.push(formTemplateArray);
+        
+*/  
+        fs.appendFile(
+           app ,
+           //pageArray.map(function(v){ return v.join(', ')).join('\n'),
+           pageArray.join('\n'),
+           function (err) { console.log(err ? 'Error :' + err : 'ok - writeFile ['+app+']') }
+        );
+     
 /*
 			var select = new views.Select();
         	var someArray = ['a','b','c'];
@@ -173,41 +261,11 @@ function getForm(){
         	jobName.value.binding = '[jobName]';
   */                  
 
+        
+      });
 
-
-
-
-			//fs.writeFile(app, JSON.stringify(jsonObj) , function (err) {
-			fs.writeFile(app, JSON.stringify(jsonObj) , function (err) {
-			  if (err) throw err;
-			  console.log(selected.appName + ' saved to /pages/ folder');
-			});
-
-			formJSON = jsonObj;
-
-			
-			
-			
-		});
-
-		//read from file
-		fileStream.pipe(csvConverter);
-		
-
-
-	/*
-		xlsxj({
-		    input: selected.file, 
-		    output: null
-		  }, function(err, result) {
-		    if(err) {
-		      console.error(err);
-		    }else {
-		      console.log(result);
-		    }
-		 });
-	*/
-
+      //read from file
+      fileStream.pipe(csvConverter);
 		});
 	});
 
@@ -231,6 +289,61 @@ fs.writeFile(app,JSON.stringify(jsonObj), function (err) {
 
 }
 
+function writePage(){
+  
+		  
+  /*
+	fs.unlink(page, function (err) {
+    if (err) throw err;
+    console.log('successfully deleted ['+page+']');
+  });
+  
+  
+  fs.unlink(json, function (err) {
+    if (err) throw err;
+    console.log('successfully deleted ['+json+']');
+  });
+  */
+  
+      
+      fs.appendFile(page, 'function createForm(){\n  var formPage = new views.Form();\n', function (err) {
+      	if (err) throw err;
+			});
+      
+      jsonObj.forEach(function(key) {
+        fs.appendFile(page, '  var ' + key.label.replace(/\s/g, '') + 'Label = new views.Label();\n  var ' + key.label.replace(/\s/g, '') + key.type + ' = new views.'+key.type+'();\n\n', function (err) {
+        	if (err) throw err;
+				});
+       });
+       
+			jsonObj.forEach(function(key) {
+        fs.appendFile(page, key.label.replace(/\s/g, '') + '.text.value = \''+ key.label +'\';\n' + key.label.replace(/\s/g, '') + '.classes.value = \''+ key.classes +'\';\n\n', function (err) {
+        	if (err) throw err;
+				});
+      });
+      
+      fs.appendFile(page, '  formPage.views.content.add([\n', function (err) {
+      	if (err) throw err;
+			});
+      
+      
+      
+		  jsonObj.forEach(function(key) {
+        
+        fs.appendFile(page, key.label.replace(/\s/g, '') + 'Label,\n' + key.label.replace(/\s/g, '') + key.type + ',\n', function (err) {
+        	if (err) throw err;
+				});
+      });
+		  fs.appendFile(page, '  ]);\n  formPage.path = \'[/form]\';', function (err) {
+      	if (err) throw err;
+			});
+
+}
+
+function ucfirst(str) {
+  var firstLetter = str.slice(0,1);
+  return firstLetter.toUpperCase() + str.substring(1).toLowerCase();
+}
 
 	runApp();
 
